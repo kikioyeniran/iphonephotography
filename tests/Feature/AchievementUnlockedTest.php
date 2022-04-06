11e447bae7c1aae478360c13842dd276b1234930
kikiope Oyeniran <kikioyeniran@gmail.com>
2022-04-06 01:33:54 +0100

    changes to achievement unlocked

--- tests/Feature/AchievementUnlockedTest.php
+++ tests/Feature/AchievementUnlockedTest.php
@@ -24,8 +24,6 @@ class AchievementUnlockedTest extends TestCase
     {
         Event::fake();
 
-        $comment = Comment::factory()->create();
-
         $user = User::inRandomOrder()->first();
 
         $achievement = Achievement::inRandomOrder()->first();
